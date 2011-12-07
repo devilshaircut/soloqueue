d7f1822567734f7fe0f6dcf9f28259373ca4eeb5
Jason Bishop <devilshaircut@gmail.com>
2011-12-07 15:54:36 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soloqueue::Application.config.secret_token = '4bd362c5fa1a7d0f771ebf23f9411bdc68c4030b5d9d9beb8cc83e3b57c36522c4cc003fecb3070a5d36d8febb6ecc4282ce2a2fa6523d44e3f14adb7d430ab2'
