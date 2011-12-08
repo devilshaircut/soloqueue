bf5b4d6807e7865ab403a3eea1f3111ea2841d1b
Cody Stewart <cjstewart88@gmail.com>
2011-12-07 22:27:30 -0500

    suggestions as you type

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,42 @@
+function search (search_input) {
+  var champs  = ["Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Blitzcrank", "Brand", "Caitlyn", "Cassiopeia", "Cho'Gath", "Corki", "Dr. Mundo", "Evelynn", "Ezreal", "Fiddlesticks", "Galio", "Gangplank", "Garen", "Gragas", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nasus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Rammus", "Renekton", "Riven", "Rumble", "Ryze", "Shaco", "Shen", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Talon", "Taric", "Teemo", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Vayne", "Veigar", "Vladimir", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yorick", "Zilean"]
+  var search_results = $("#search-results");
+
+  for (var i = 0; i < champs.length; i++) {
+    if (champs[i].toLowerCase().indexOf(search_input.toLowerCase()) != -1) {
+     if (search_results.find("#" + i).length != 1) {
+       search_results.append("<li id='" + i + "'>" + champs[i] + "</li>"); 
+     }
+    }
+    else {
+      search_results.find("#" + i).remove();
+    }
+  }
+  
+  if (search_results.length > 0) {
+    search_results.show();
+  }
+  else {
+    search_results.hide();
+  }
+}
+
+function set_search (search_input) {
+  $("#search").val(search_input);
+  $("#search-results").html('').hide();
+}
+
+$(document).ready(function () {
+  var timer;
+  $("#search").keyup(function () {
+    clearTimeout(timer);
+    var search_input = $(this).val();
+    timer = setTimeout(function () {
+      search(search_input);
+    }, 500);
+  });
+  
+  $("#search-results").delegate("li", "click", function () {
+    set_search($(this).html());
+  });
+});
