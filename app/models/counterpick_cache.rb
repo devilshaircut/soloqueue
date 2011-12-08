2b2004f69753fc1c76639615c9f3b455a542f64a
Jason Bishop <devilshaircut@gmail.com>
2011-12-07 21:36:29 -0500

    Make the counterpick cache feature work.

--- app/models/counterpick_cache.rb
+++ app/models/counterpick_cache.rb
@@ -4,12 +4,11 @@ class Counterpicks
 end
 
 class CounterpickCache < ActiveRecord::Base
-  
-  attr_accessor :latestcounterpick
     
   # Get a the list of champs from the Google Spreadsheet API, then update the cache.
   def updateCounterpickCache
-    CounterpickCache.find(1).latestcounterpick = Counterpicks.get('https://spreadsheets.google.com/feeds/list/0AvFI-VeUB6LddEtiY3RqQUg2eGlLMEpMN2llN0dsVGc/od6/public/values').as_json['feed']['entry']
+    self.latestcounterpick = Counterpicks.get('https://spreadsheets.google.com/feeds/list/0AvFI-VeUB6LddEtiY3RqQUg2eGlLMEpMN2llN0dsVGc/od6/public/values').as_json['feed']['entry']
+    self.save
   end
   
 end
