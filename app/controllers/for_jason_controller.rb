15e9a4f60d895f48e9346a1bbfa918b2d6c25cfd
Jason Bishop <devilshaircut@gmail.com>
2011-12-08 19:44:53 -0500

    Parse out inline styles.

--- app/controllers/for_jason_controller.rb
+++ app/controllers/for_jason_controller.rb
@@ -4,13 +4,27 @@ class ForJasonController < ApplicationController
 
   def for_jason
     
+    # Given a champion name, return his or her skills from Wikia.
     def findSkills(champion)
+      
+      # Construct the URL from params and make the page request.
       url = "http://leagueoflegends.wikia.com/wiki/"
       champ = champion.to_s
       wikiaList = HTTParty.get(url << champ)
-      return Hpricot(wikiaList).search(".abilities_table").to_html
+      
+      # Extract the abilities table from the page.
+      skills = Hpricot(wikiaList).search(".abilities_table")
+      
+      # Remove inline styles.
+      skills.search("[@style]").each do |e|
+        e.remove_attribute("style")
+      end
+      
+      return skills.to_html
+      
     end
     
+    # Run the findSkills method.
     @jason = findSkills(params["champions"])
     
   end
