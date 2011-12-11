443e99c9665f437993bea03c12706f5e12e4c0f1
Cody Stewart <cjstewart88@gmail.com>
2011-12-10 20:52:19 -0500

    header

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -82,7 +82,7 @@ function get_data (search) {
 }
 
 function setListHolderHeight () {
-  $("#list-holder").height($(window).height()-132 + "px");
+  $("#list-holder").height($(window).height()-142 + "px");
 }
 
 $(document).ready(function () {
