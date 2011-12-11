f720409c55976244512f6dc0c5012c7414716cbe
Cody Stewart <cjstewart88@gmail.com>
2011-12-11 01:25:48 -0500

    about page and some styling.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,31 +3,9 @@ class HomeController < ApplicationController
   def index
     response.headers['Cache-Control'] = 'public, max-age=3600' if Rails.env.production?
   end
-  
-  def test
-    tmp = HTTParty.get("http://leagueoflegends.wikia.com/api.php",{
-      :query => {
-        :action=>:query,
-        :titles=>"Akali the Fist of Shadow",
-        :prop=>:revisions,
-        :rvprop=>:content,
-        :format=>:json
-      }
-    })
-    Rails.logger.debug tmp.body
-    json = JSON.parse(tmp.body)
-    @output = json["query"]["pages"]["4994"]["revisions"][0]["*"]
-    tmp1 = HTTParty.post("http://leagueoflegends.wikia.com/api.php", {
-      :query => {
-        :action => :parse,
-        :text => @output
-      }
-    })
-    @output2 = tmp1.body
-    # @output1 = JSON.parse(tmp1.body)
-    # Rails.logger.debug @output1.inspect
-  end
 
+  def about 
+  end
 end
 
 
