2647569ecdd94fe11b52d99cfb7c9d726cd2100a
Jason Bishop <devilshaircut@gmail.com>
2012-01-03 17:10:22 -0500

    Add in beginnings of the masteries calculator feature. Markup shell stage 1.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Soloqueue::Application.routes.draw do
   
   get "about" => "home#about"
   get "damage-calculator" => "home#damage_calculator"
+  get "masteries" => "home#masteries"
     
   get "/api/:input_name.json" => "api#fetch_data"
   
